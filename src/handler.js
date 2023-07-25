60dc1a94897d7b646a544cb8f031eb1e1730e70d
Nexvaulus <moehammad.fikr@gmail.com>
2023-07-25 18:39:52 +0700

    Add handler.js & books.js

---
+++ src/handler.js
@@ -0,0 +1,14 @@
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+};
+
+module.exports = {addBookHandler};
